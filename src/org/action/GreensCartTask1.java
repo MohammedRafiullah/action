e6a5fe34808f81534514cd4722542239f44e3b17
Mohammed <mohammedrafiullah16@gmail.com>
2022-07-29 16:24:12 +0530

    yes

---
+++ src/org/action/GreensCartTask1.java
@@ -0,0 +1,51 @@
+package org.action;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GreensCartTask1 {
+	
+	public static void main(String[] args) throws InterruptedException {
+    System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\Mohammed Rafiullah\\\\eclipse-workspace\\\\Selenium Project\\\\driver\\\\chromedriver.exe");
+    
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://greenscart.in/");
+    driver.manage().window().maximize();
+    Thread.sleep(3000);
+   
+    
+    WebElement username = driver.findElement(By.xpath("//input[@id='loginEmailId']"));
+    username.sendKeys("mohammedrafiullah16@gmail.com");
+    Thread.sleep(3000);
+    
+   WebElement Pass = driver.findElement(By.xpath("//input[@id='loginPassword']"));
+    Pass.sendKeys("Symphony@16");
+    Thread.sleep(3000);
+    
+    WebElement login = driver.findElement(By.xpath("//input[@class='btnRegister form-control']"));
+    Pass.click();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+		
+		
+	}
+
+}
